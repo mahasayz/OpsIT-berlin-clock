9e7b16b787f907e926e305c5c181e1ba00612b65
Mahbub Alam <mahasayz@gmail.com>
2015-07-07 11:56:35 +0600

    Refactored code. Replaced time split on : with Calendar parsing

--- src/main/java/com/ubs/opsit/interviews/BerlinClock.java
+++ src/main/java/com/ubs/opsit/interviews/BerlinClock.java
@@ -1,27 +1,58 @@
 package com.ubs.opsit.interviews;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class BerlinClock implements TimeConverter{
 
-	private static List<TimeUnit> timeUnits = new ArrayList<TimeUnit>();
-	
-	public BerlinClock() {
-		timeUnits.add(new Second());
-		timeUnits.add(new Hour());
-		timeUnits.add(new Minute());
-	}
-	
 	@Override
 	public String convertTime(String aTime) {
 		// TODO Auto-generated method stub
 		StringBuilder timeSB = new StringBuilder();
-		String[] timeElements = aTime.split(":");
 		
-		timeSB.append(timeUnits.get(0).getLamps(Integer.parseInt(timeElements[2]))).append(System.lineSeparator());
-		timeSB.append(timeUnits.get(1).getLamps(Integer.parseInt(timeElements[0]))).append(System.lineSeparator());
-		timeSB.append(timeUnits.get(2).getLamps(Integer.parseInt(timeElements[1])));
+		SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+		Calendar cal = Calendar.getInstance();
+		
+		try {
+			cal.setTime(formatter.parse(aTime));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		/**
+		 * Finding the following scenario ambiguous in case of Berlin Clock
+		 * 12 Midnight can be represented both as 00:00:00 or 24:00:00
+		 * 
+		 * Both time formats mean the same but have different representations in
+		 * terms of lamp light-up mechanisms, like
+		 * 
+		 * 00:00:00 is represented as
+		 * Y
+		 * 0000
+		 * 0000
+		 * 00000000000
+		 * 0000
+		 * 
+		 * 24:00:00 is represented as
+		 * Y
+		 * RRRR
+		 * RRRR
+		 * 00000000000
+		 * 0000
+		 */
+		
+		String hour = aTime.substring(0, aTime.indexOf(":"));
+		
+		timeSB.append(new Second().getLamps(cal.get(Calendar.SECOND))).append(System.lineSeparator());
+		if (hour.equals("24"))
+			timeSB.append(new Hour().getLamps(24)).append(System.lineSeparator());
+		else
+			timeSB.append(new Hour().getLamps(cal.get(Calendar.HOUR_OF_DAY))).append(System.lineSeparator());
+		timeSB.append(new Minute().getLamps(cal.get(Calendar.MINUTE)));
 		
 		return timeSB.toString();
 	}
